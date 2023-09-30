daa2ea0ca8e4de0da2026cdc1792651e22ebdb78
Tokyo <crackedthoughhh@gmail.com>
2023-09-30 21:33:17 +0100

    added
    +drop shadow
    +type music
    +blurr
    -rainbow text

--- music.js
+++ music.js
@@ -1,9 +1,27 @@
-// Get the username input element and login sound element
+// Get the username and password input elements and login sound element
 const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
 const loginSound = document.getElementById("loginSound");
 
-// Add an input event listener to the username input
-usernameInput.addEventListener("input", () => {
-    // Play the login sound
-    loginSound.play();
-});
+// Create an array to store audio elements
+const audioElements = [];
+
+// Function to handle input events and play the sound
+function playLoginSound() {
+    const newLoginSound = loginSound.cloneNode(true);
+
+    audioElements.push(newLoginSound);
+
+    newLoginSound.play();
+
+    newLoginSound.addEventListener("ended", () => {
+        const index = audioElements.indexOf(newLoginSound);
+        if (index !== -1) {
+            audioElements.splice(index, 1);
+        }
+    });
+}
+
+// Add input event listeners to both username and password inputs
+usernameInput.addEventListener("input", playLoginSound);
+passwordInput.addEventListener("input", playLoginSound);
